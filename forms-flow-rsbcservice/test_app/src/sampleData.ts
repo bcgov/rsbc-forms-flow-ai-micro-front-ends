f3d077b43e3055c026ce65ac883d39a37152baef
Adimar Borges <adimar.borges@aot-technologies.com>
2026-04-16 09:41:59 -0700

    Update IRP Driver copy mapping

--- forms-flow-rsbcservice/test_app/src/sampleData.ts
+++ forms-flow-rsbcservice/test_app/src/sampleData.ts
@@ -22,10 +22,10 @@ export const sampleData: Record<string, any> = {
   // Driver information
   driver_licence_no: "334324",
   drivers_licence_jurisdiction: "BC",
-  driver_last_name: "Doe",
-  driver_given_name: "John",
-  driver_address: "1934 Westpark Ln",
-  driver_city: "Victoria",
+  driver_last_name: "DOE",
+  driver_given_name: "JOHN",
+  driver_address: "1934 WESTPARK LN",
+  driver_city: "VICTORIA",
   driver_prov_state: "BC",
   driver_postal: "V5K 2J1",
   driver_dob: "1985-06-15",
@@ -35,32 +35,36 @@ export const sampleData: Record<string, any> = {
   gender: "M",
 
   // Location information
-  intersection_or_address_of_offence: "Douglas St & Fort St",
-  offence_city: "Victoria",
+  intersection_or_address_of_offence: "DOUGLAS ST & FORT ST",
+  offence_city: "VICTORIA",
   offence_prov_state: "BC",
 
   // Vehicle information
   licencePlateNumber: "ABC-123",
   provinceState: "BC",
   vehicleYear: "2020",
-  vehicleMakeAndModel: "Toyota Camry",
+  vehicleMakeAndModel: "TOYOTA CAMRY",
   vehicleColours: "BLK",
   vehicleIdentificationNumberVin1: "1HGBH41JXMN109186",
 
   // Officer / incident information
-  officer_last_name: "Smith",
-  officer_badge_number: "12345",
+  "officer-lastname": "SMITH",
+  "officer-prime-id": "VI3833",
+  "officer-agency": "VICTORIA POLICE DEPARTMENT",
+  agency_file_no: "VPD-2025-0001",
   date_of_driving: "2025-04-14",
   time_of_driving: "14:30",
   prohibitionType: "24hour",
   roadSafetyAct: true,
+  confirmation_of_service_date: "2025-04-14",
 
   // Impound information
   vehicle_impounded: "NO",
   date_of_impound: "2025-04-14",
 
   // IRP-specific fields
-  seized_DL: "NO"
+  seized_DL: "NO",
+  irp_impound_duration: "BACREFUSAL",
 };
 
 /**
