8f0c4a45b46edac552a7efd17bed80b646e42349
Adimar Borges <adimar.borges@aot-technologies.com>
2026-07-24 14:22:21 -0700

    Update RTS layout

--- forms-flow-rsbcservice/src/helpers/helperServices.ts
+++ forms-flow-rsbcservice/src/helpers/helperServices.ts
@@ -126,6 +126,7 @@ const formsPNGVersion2: FormsPNG = {
   },
   rts: {
     IRP: {
+      POLICE: { png: irpForm, aspectClass: "--portrait" },      
       RTS: { png: rtsForm, aspectClass: "--portrait" },
       RTS_BACK: { png: rtsBackForm, aspectClass: "--portrait-upside-down" },
     },
